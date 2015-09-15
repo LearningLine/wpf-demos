b7c060a3503c4d562486fddb3efa476df9f30b25
Brad Cunningham <Brad@BradCunningham.net>
2015-09-15 08:45:08 -0700

    Add Day 2 Student content. Fix organization of Student content on Day1.

--- Day1/Student Content/Day 5/Labs/Design Patterns for Testability/VS2012/DoublesAndMocking/after/TFSBlogRepository/BlogModel.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'Z:\Desktop\CareStream Course\Student Materials\20_Design Patterns for Testability\Lab\VS2012\DoublesAndMocking\after\TFSBlogRepository\BlogModel.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
