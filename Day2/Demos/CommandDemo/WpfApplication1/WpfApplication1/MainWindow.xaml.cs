43cded834e7c89bf29a1e0838cd5e39efa6d9250
Brad Cunningham <Brad@BradCunningham.net>
2015-09-15 14:46:18 -0700

    Updates for Events and Commands

---
+++ Day2/Demos/CommandDemo/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfApplication1
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.InputBindings.Add(new InputBinding(FormatCommands.SetBackground,
+				new KeyGesture(Key.K, ModifierKeys.Control)));
+		}
+
+		private void CommandBinding_OnCanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			RichTextBox rtb = (RichTextBox) sender;
+			if (rtb.Selection.IsEmpty)
+			{
+				e.CanExecute = false;
+			}
+			else
+			{
+				e.CanExecute = true;
+			}
+		}
+
+		private void CommandBinding_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			RichTextBox rtb = (RichTextBox)sender;
+			rtb.Selection.ApplyPropertyValue(TextElement.BackgroundProperty,
+				Brushes.Green);
+
+		}
+	}
+}
---
+++ Day2/Demos/EventDemo/EventDemo/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EventDemo
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.PreviewMouseDown += MainWindow_PreviewMouseDown;
+			button3.Click += Button3_Click;
+		}
+
+		private void MainWindow_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			if (e.Source is Button)
+			{
+				Console.WriteLine("About to click a button");
+				//e.Handled = true;
+			}
+			else
+			{
+				Console.WriteLine("You clicked something elese");
+			}
+		}
+
+		private void Button3_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Clicked button 3");
+			e.Handled = true;
+		}
+
+		private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("You clicked a button");
+		}
+
+		private void MainWindow_OnClick(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("I am the windows eating your clicks");
+		}
+	}
+}
