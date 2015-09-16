5c3aa6be2430dcec96055459ba9dc7b29a7ac86a
Brad Cunningham <Brad@BradCunningham.net>
2015-09-16 14:10:15 -0700

    Update after 2nd Data Binding discussion

---
+++ Day3/Demos/BindingDemo2/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		ProductList productsList = new ProductList();
+		public MainWindow()
+		{
+			InitializeComponent();
+			DataContext = productsList;
+		}
+
+		private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+		{
+			//productsList.Products[0].Price = 500;
+			productsList.Products.Add(new Product
+			{
+				Type = "iPad",
+				Price = 999999
+			});
+		}
+
+	
+		private void FilterItems(object sender, RoutedEventArgs e)
+		{
+			ICollectionView cv = CollectionViewSource.GetDefaultView(productsList.Products);
+			cv.Filter = o =>
+			{
+				var product = (Product) o;
+				return product.Price < 100;
+			};
+			cv.SortDescriptions.Add(new SortDescription("Type",ListSortDirection.Ascending));
+		}
+
+		private void UnFilterItems(object sender, RoutedEventArgs e)
+		{
+			ICollectionView cv = CollectionViewSource.GetDefaultView(productsList.Products);
+			cv.Filter = null;
+			cv.SortDescriptions.Clear();
+		}
+	}
+}
---
+++ Day3/Demos/BindingDemo2/WpfApplication1/WpfApplication1/Product.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security.Permissions;
+using System.Windows.Data;
+using WpfApplication1.Annotations;
+
+namespace WpfApplication1
+{
+	//[DebuggerDisplay(Price,"Price")]
+	public class Product : INotifyPropertyChanged
+	{
+		private int _price;
+		public string Type { get; set; }
+
+		public int Price
+		{
+			get { return _price; }
+			set
+			{
+				_price = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public override string ToString()
+		{
+			return string.Format("{0} {1}", Type, Price);
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		[NotifyPropertyChangedInvocator]
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+
+	public class ProductList : INotifyPropertyChanged
+	{
+		private string _search;
+		public ObservableCollection<Product> Products { get; set; }
+
+		public string Search
+		{
+			get { return _search; }
+			set
+			{
+				_search = value;
+
+				ICollectionView cv = CollectionViewSource.GetDefaultView(Products);
+				cv.Filter = o =>
+				{
+					var product = (Product)o;
+					return product.Type.Contains(Search);
+				};
+				OnPropertyChanged();
+
+			}
+		}
+
+		public ProductList()
+		{
+			Products = new ObservableCollection<Product>
+			{
+				new Product
+				{
+					Type = "Mouse",
+					Price = 10
+				},
+				new Product
+				{
+					Type = "Keyboard",
+					Price = 5
+				}
+			};
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		[NotifyPropertyChangedInvocator]
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
