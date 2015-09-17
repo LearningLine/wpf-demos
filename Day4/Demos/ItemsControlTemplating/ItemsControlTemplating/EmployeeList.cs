d26ae5c4c3700784c34f2464cc370bca11b86914
Brad Cunningham <Brad@BradCunningham.net>
2015-09-17 14:01:14 -0700

    Update after CT talk 2

---
+++ Day4/Demos/ItemsControlTemplating/ItemsControlTemplating/EmployeeList.cs
@@ -0,0 +1,37 @@
+﻿using System.CodeDom;
+using System.Collections.ObjectModel;
+
+namespace ItemsControlTemplating
+{
+	public class EmployeeList
+	{
+		private ObservableCollection<Employee> _employees;
+
+		public ObservableCollection<Employee> Employees
+		{
+			get { return _employees; }
+			set { _employees = value; }
+		}
+
+		public EmployeeList()
+		{
+			Employees = new ObservableCollection<Employee>();
+			Employees.Add(new Employee
+			{
+				FirstName = "Brad",
+				LastName = "Cunningham"
+			});
+			Employees.Add(new Employee
+			{
+				FirstName = "Joe",
+				LastName = "Smith"
+			});
+		}
+	}
+
+	public class Employee
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+}
